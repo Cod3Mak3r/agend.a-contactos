ae01c5ca15010b06d6a01f292e4ac2fb26a15783
kenneth <hkenneth939@gmail.com>
2021-11-08 15:26:57 -0500

    Se creo la entidad contacto

---
+++ src/main/java/com/contactos/agenda/app/entities/Contacto.java
@@ -0,0 +1,70 @@
+package com.contactos.agenda.app.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="contactos")
+public class Contacto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long   idContacto;
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private String email;
+	private String mensaje;
+	
+	
+	
+	public long getIdContacto() {
+		return idContacto;
+	}
+	public void setIdContacto(long idContacto) {
+		this.idContacto = idContacto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+}
