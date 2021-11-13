bc6367df816eb4013d5102f1e6c32bd1328236e4
kenneth <hkenneth939@gmail.com>
2021-11-13 13:40:14 -0500

    Se mejoro el API REST, agregando un verbo distinto a cada metodo. Haciendolo mas limpio y legible

--- src/main/java/com/contactos/agenda/app/controllers/ContactoControllerRest.java
+++ src/main/java/com/contactos/agenda/app/controllers/ContactoControllerRest.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.contactos.agenda.app.entities.Contacto;
@@ -18,46 +22,52 @@ import com.contactos.agenda.app.services.IContactoService;
 @RestController
 @RequestMapping("/api/v1/contactos")
 public class ContactoControllerRest {
-	
+
 	@Autowired
 	private IContactoService contactoService;
-	
-	//List all contacts
-	@GetMapping("/list")
-	public List<Contacto> list(){
+
+	// List all contacts
+	@GetMapping
+	public List<Contacto> list() {
 		return contactoService.listarContactos();
 	}
-	
-	
-	//Create and update a new contact
-	@PostMapping("/save")
-	public ResponseEntity<Contacto> create(@RequestBody Contacto contacto){
-		Contacto contact  = contactoService.guardarContacto(contacto);
-		return new ResponseEntity<Contacto>(contact, HttpStatus.OK);	
-	}
-	
-	//Read an contact
-	@GetMapping("/find/{id}")
-	public ResponseEntity<Contacto> find(@PathVariable Long id){
+
+	// Read an contact
+	@GetMapping("/{id}")
+	public ResponseEntity<Contacto> find(@PathVariable Long id) {
 		var contacto = contactoService.buscarContacto(id);
-		 
 		if (contacto == null) {return ResponseEntity.notFound().build();}
-		
 		return ResponseEntity.ok(contacto);
 	}
-	
-	//Delete an contact
-	@PostMapping("/delete/{id}")
-	public ResponseEntity<Contacto> delete(@PathVariable Long id){
-		Contacto contact = contactoService.buscarContacto(id);
-		if (contact != null) {contactoService.eliminarContacto(id);}
-		else
-		{ return new ResponseEntity<Contacto>
-							(contact, HttpStatus.INTERNAL_SERVER_ERROR);}
-		
+
+	// Create a new contact
+	@PostMapping
+	public ResponseEntity<Contacto> create(@RequestBody Contacto contacto) {
+		Contacto contact = contactoService.guardarContacto(contacto);
 		return new ResponseEntity<Contacto>(contact, HttpStatus.OK);
 	}
-	
-	
-	
+
+	// Update an contact
+	@PutMapping("/{id}")
+	@ResponseBody
+	public ResponseEntity<Contacto> update(@PathVariable Long id, 
+											@RequestBody Contacto contacto) {
+		contactoService.buscarContacto(id);
+		Contacto contact = contactoService.actualizarContacto(contacto);
+		return new ResponseEntity<Contacto>(contact, HttpStatus.OK);
+	}
+
+	// Delete an contact
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Contacto> delete(@PathVariable Long id) {
+		Contacto contact = contactoService.buscarContacto(id);
+		if (contact != null) {
+			contactoService.eliminarContacto(id);
+		} else {
+			return new ResponseEntity<Contacto>(contact, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<Contacto>(contact, HttpStatus.OK);
+	}
+
 }
